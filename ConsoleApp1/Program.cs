933c307e9ef8dce155cae6d95d8266566d6604fd
divyamj <divyamj93@gmail.com>
2024-03-27 11:13:03 +0530

    task1

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //task
+
+            // 1. .Define variable with var and assign different data type values
+            var variable1 = 10;
+            var variable2 = 3.14;
+            var variable3 = "Hello";
+            var variable4 = true;
+            Console.WriteLine("Data type of variable1: " + variable1.GetType());
+            Console.WriteLine("Data type of variable2: " + variable2.GetType());
+            Console.WriteLine("Data type of variable3: " + variable3.GetType());
+            Console.WriteLine("Data type of variable4: " + variable4.GetType());
+
+            //2. Accept 3 numbers
+
+            Console.WriteLine("Enter three numbers:");
+
+            Console.Write("Number 1: ");
+            int num1 = Convert.ToInt32(Console.ReadLine());
+
+            Console.Write("Number 2: ");
+            int num2 = Convert.ToInt32(Console.ReadLine());
+
+            Console.Write("Number 3: ");
+            int num3 = Convert.ToInt32(Console.ReadLine());
+            int greatest = num1;
+            if (num2 > greatest)
+            {
+                greatest = num2;
+            }
+            if (num3 > greatest)
+            {
+                greatest = num3;
+            }
+
+            Console.WriteLine($"The greatest number is: {greatest}");
+
+            //3.Accept an alphabet and print whether it is vowel or not.(use switch...case)
+
+            Console.WriteLine("Enter an alphabet:");
+            char alphabet = Console.ReadLine().ToLower()[0];
+
+            switch (alphabet)
+            {
+                case 'a':
+                    Console.WriteLine("The entered alphabet is a vowel.");
+                    break;
+                case 'e':
+                    Console.WriteLine("The entered alphabet is a vowel.");
+                    break;
+                case 'i':
+                    Console.WriteLine("The entered alphabet is a vowel.");
+                    break;
+                case 'o':
+                    Console.WriteLine("The entered alphabet is a vowel.");
+                    break;
+                case 'u':
+                    Console.WriteLine("The entered alphabet is a vowel.");
+                    break;
+                default:
+                    Console.WriteLine("The entered alphabet is not a vowel.");
+                    break;
+            }
+            Console.ReadLine();
+        }
+    }
+}
